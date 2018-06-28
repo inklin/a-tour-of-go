3c999c521abcd4162cf436b31c99c8c5e42244c0
Stacey Lin <twstacey@gmail.com>
2018-06-28 14:46:19 -0700

    Add new file for pointer reference methods

--- interface-values/main.go
+++ interface-values/main.go
@@ -3,48 +3,48 @@ package main
 import "fmt"
 
 type Character struct {
-  name string
-  age int
+	name string
+	age  int
 }
 
 type Wizard struct {
-  Character
-  powerLevel int
+	Character
+	powerLevel int
 }
 
 type Human struct {
-  Character
+	Character
 }
 
 type Player interface {
-  growOlder()
-  selfIntro()
+	growOlder()
+	selfIntro()
 }
 
 // All characters have a self intro function
 func (c Character) selfIntro() {
-  fmt.Println("My name is", c.name, "and my age:", c.age)
+	fmt.Println("My name is", c.name, "and my age:", c.age)
 }
 
 // Wizard has a growOlder function
-// 
+//
 // Note that since this is a non-pointer reference,
 // we cannot change the value, we can only read
 func (w Wizard) growOlder() {
-  fmt.Println("Wizards don't age, I will be forever", w.age, "because of power level:", w.powerLevel)
+	fmt.Println("Wizards don't age, I will be forever", w.age, "because of power level:", w.powerLevel)
 }
 
 // Human has a growOlder function
 // mutates the age, increasing the age by 1
 func (h *Human) growOlder() {
-  h.age++
+	h.age++
 }
 
 func main() {
-  var merlin Player
-  var bob Player
+	var merlin Player
+	var bob Player
 
-  merlin = Wizard{Character{"Merlin", 100}, 1000}
+	merlin = Wizard{Character{"Merlin", 100}, 1000}
 
 	// Calling a method on the interface Player
 	// executes the method of the same name on the underlying type.
@@ -53,20 +53,20 @@ func main() {
 	// on type Wizard is called.
 	merlin.growOlder()
 	// When we call selfIntro again, we see that merlin's age is unchanged
-  merlin.selfIntro()
+	merlin.selfIntro()
 
-  // Note that Human needs to implement
-  // a pointer receiver to fulfill the interface of Player
-  // that needs a growOlder() function
-  bob = &Human{Character{"Bob", 30}}
+	// Note that Human needs to implement
+	// a pointer receiver to fulfill the interface of Player
+	// that needs a growOlder() function
+	bob = &Human{Character{"Bob", 30}}
 
 	// Initially Bob has an age of 30, which we initialized.
 	bob.selfIntro()
 	// The underlying type is Human, and the growOlder function on type Human is called.
-	// Bob's age is mutated and increased by 1. 
+	// Bob's age is mutated and increased by 1.
 	bob.growOlder()
 	// When we call selfIntro again, we see that bob's age has increased to 31.
-  bob.selfIntro()
+	bob.selfIntro()
 }
 
 // Output from this program:
--- pointer-reference-methods/main.go
+++ pointer-reference-methods/main.go
@@ -1,7 +1,7 @@
 package main
 
-/* Pointer VS. Non-Pointer receiver methods 
- * 
+/* Pointer VS. Non-Pointer receiver methods
+ *
  * Why pass a pointer ? (Why would you want to pass by reference instead of value)
  * - you want to modify the underlying values
  * - the struct is large and deep copying would be expensive
@@ -13,32 +13,32 @@ package main
 import "fmt"
 
 type Coordinate struct {
-  x int
-  y int
+	x int
+	y int
 }
 
 // Pass by value, non-pointer receiver
 // this will not change the value of x and y
 func (c Coordinate) shouldNotMutate() {
-  c.x = 100
-  c.y = 200
+	c.x = 100
+	c.y = 200
 }
 
 // Pointer receiver
 // This will allow you to access and change
 // the underlying value of the pointer
 func (c *Coordinate) shouldMutate() {
-  c.x = 1000
-  c.y = 1000
+	c.x = 1000
+	c.y = 1000
 }
 
 func main() {
-  coordinate := Coordinate{25, 35}
-  fmt.Println("Original values of x,y: ", coordinate.x, coordinate.y)
+	coordinate := Coordinate{25, 35}
+	fmt.Println("Original values of x,y: ", coordinate.x, coordinate.y)
 
-  coordinate.shouldNotMutate()
-  fmt.Println("After calling shouldNotMutate():", coordinate.x, coordinate.y)
+	coordinate.shouldNotMutate()
+	fmt.Println("After calling shouldNotMutate():", coordinate.x, coordinate.y)
 
-  coordinate.shouldMutate()
-  fmt.Println("After calling shouldMutate():", coordinate.x, coordinate.y)  
+	coordinate.shouldMutate()
+	fmt.Println("After calling shouldMutate():", coordinate.x, coordinate.y)
 }
